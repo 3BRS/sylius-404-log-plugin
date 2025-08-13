1af9473e69a9b8c27ae5db327f7848632057450e
Ondřej Kühnel <ondrej@3brs.com>
2025-08-13 10:56:17 +0200

    remove old logs

--- tests/Unit/EventListener/ExceptionLoggerListenerTest.php
+++ tests/Unit/EventListener/ExceptionLoggerListenerTest.php
@@ -12,29 +12,22 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
-use ThreeBRS\Sylius404LogPlugin\Entity\NotFoundLogInterface;
 use ThreeBRS\Sylius404LogPlugin\EventListener\ExceptionLoggerListener;
-use ThreeBRS\Sylius404LogPlugin\Factory\NotFoundLogFactoryInterface;
 
 class ExceptionLoggerListenerTest extends TestCase
 {
     private MockObject|LoggerInterface $logger;
     private MockObject|EntityManagerInterface $entityManager;
-    private MockObject|NotFoundLogFactoryInterface $notFoundLogFactory;
-    private MockObject|NotFoundLogInterface $notFoundLog;
     private ExceptionLoggerListener $listener;
 
     protected function setUp(): void
     {
         $this->logger = $this->createMock(LoggerInterface::class);
         $this->entityManager = $this->createMock(EntityManagerInterface::class);
-        $this->notFoundLogFactory = $this->createMock(NotFoundLogFactoryInterface::class);
-        $this->notFoundLog = $this->createMock(NotFoundLogInterface::class);
 
         $this->listener = new ExceptionLoggerListener(
             $this->logger,
             $this->entityManager,
-            $this->notFoundLogFactory,
             ['/admin', '/api']
         );
     }
@@ -50,7 +43,6 @@ class ExceptionLoggerListenerTest extends TestCase
             $exception
         );
 
-        $this->notFoundLogFactory->expects($this->never())->method('createNew');
         $this->entityManager->expects($this->never())->method('persist');
 
         $this->listener->onKernelException($event);
@@ -73,29 +65,9 @@ class ExceptionLoggerListenerTest extends TestCase
             $exception
         );
 
-        $this->notFoundLogFactory->expects($this->once())
-            ->method('createNew')
-            ->willReturn($this->notFoundLog);
-
-        $this->notFoundLog->expects($this->once())
-            ->method('setUrlDomain')
-            ->with('example.com');
-
-        $this->notFoundLog->expects($this->once())
-            ->method('setUrlSlug')
-            ->with('/some-page');
-
-        $this->notFoundLog->expects($this->once())
-            ->method('setQueryString')
-            ->with(null);
-
-        $this->notFoundLog->expects($this->once())
-            ->method('setUserAgent')
-            ->with('Mozilla/5.0');
-
         $this->entityManager->expects($this->once())
             ->method('persist')
-            ->with($this->notFoundLog);
+            ->with($this->isInstanceOf(\ThreeBRS\Sylius404LogPlugin\Entity\NotFoundLog::class));
 
         $this->entityManager->expects($this->once())
             ->method('flush');
@@ -106,10 +78,7 @@ class ExceptionLoggerListenerTest extends TestCase
     public function testOnKernelExceptionSkipsAdminPaths(): void
     {
         $exception = new NotFoundHttpException('Page not found');
-        $request = new Request([], [], [], [], [], [
-            'REQUEST_URI' => '/admin/some-page',
-            'HTTP_HOST' => 'example.com'
-        ]);
+        $request = new Request();
         $request->server->set('REQUEST_URI', '/admin/some-page');
 
         $event = new ExceptionEvent(
@@ -119,7 +88,6 @@ class ExceptionLoggerListenerTest extends TestCase
             $exception
         );
 
-        $this->notFoundLogFactory->expects($this->never())->method('createNew');
         $this->entityManager->expects($this->never())->method('persist');
 
         $this->listener->onKernelException($event);
@@ -128,11 +96,8 @@ class ExceptionLoggerListenerTest extends TestCase
     public function testOnKernelExceptionSkipsApiPaths(): void
     {
         $exception = new NotFoundHttpException('Page not found');
-        $request = new Request([], [], [], [], [], [
-            'REQUEST_URI' => '/api/v1/products',
-            'HTTP_HOST' => 'example.com'
-        ]);
-        $request->server->set('REQUEST_URI', '/api/v1/products');
+        $request = new Request();
+        $request->server->set('REQUEST_URI', '/api/some-endpoint');
 
         $event = new ExceptionEvent(
             $this->createMock(HttpKernelInterface::class),
@@ -141,7 +106,6 @@ class ExceptionLoggerListenerTest extends TestCase
             $exception
         );
 
-        $this->notFoundLogFactory->expects($this->never())->method('createNew');
         $this->entityManager->expects($this->never())->method('persist');
 
         $this->listener->onKernelException($event);
@@ -151,11 +115,8 @@ class ExceptionLoggerListenerTest extends TestCase
     {
         $exception = new NotFoundHttpException('Page not found');
         $request = new Request([], [], [], [], [], [
-            'REQUEST_URI' => '/some-page',
             'HTTP_HOST' => 'example.com',
-            'HTTP_USER_AGENT' => 'Mozilla/5.0'
         ]);
-        $request->server->set('REQUEST_URI', '/some-page');
 
         $event = new ExceptionEvent(
             $this->createMock(HttpKernelInterface::class),
@@ -164,24 +125,13 @@ class ExceptionLoggerListenerTest extends TestCase
             $exception
         );
 
-        $this->notFoundLogFactory->expects($this->once())
-            ->method('createNew')
-            ->willReturn($this->notFoundLog);
-
         $this->entityManager->expects($this->once())
             ->method('persist')
             ->willThrowException(new \Exception('Database error'));
 
         $this->logger->expects($this->once())
             ->method('error')
-            ->with(
-                'Failed to log 404 error',
-                $this->callback(function ($context) {
-                    return isset($context['exception']) &&
-                           isset($context['url']) &&
-                           isset($context['user_agent']);
-                })
-            );
+            ->with('Failed to log 404 error', $this->isType('array'));
 
         $this->listener->onKernelException($event);
     }
@@ -191,12 +141,9 @@ class ExceptionLoggerListenerTest extends TestCase
         $exception = new NotFoundHttpException('Page not found');
         $request = new Request(['param' => 'value'], [], [], [], [], [
             'REQUEST_URI' => '/some-page?param=value',
-            'QUERY_STRING' => 'param=value',
             'HTTP_HOST' => 'example.com',
-            'HTTP_USER_AGENT' => 'Mozilla/5.0'
+            'QUERY_STRING' => 'param=value'
         ]);
-        $request->server->set('REQUEST_URI', '/some-page');
-        $request->server->set('QUERY_STRING', 'param=value');
 
         $event = new ExceptionEvent(
             $this->createMock(HttpKernelInterface::class),
@@ -205,16 +152,12 @@ class ExceptionLoggerListenerTest extends TestCase
             $exception
         );
 
-        $this->notFoundLogFactory->expects($this->once())
-            ->method('createNew')
-            ->willReturn($this->notFoundLog);
+        $this->entityManager->expects($this->once())
+            ->method('persist')
+            ->with($this->isInstanceOf(\ThreeBRS\Sylius404LogPlugin\Entity\NotFoundLog::class));
 
-        $this->notFoundLog->expects($this->once())
-            ->method('setQueryString')
-            ->with('param=value');
-
-        $this->entityManager->expects($this->once())->method('persist');
-        $this->entityManager->expects($this->once())->method('flush');
+        $this->entityManager->expects($this->once())
+            ->method('flush');
 
         $this->listener->onKernelException($event);
     }
