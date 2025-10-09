e2cf1643c94b9c27b0124c777de33bef253d82ff
Jaroslav Týc <mail@jaroslavtyc.com>
2025-10-09 10:53:58 +0200

    Use indexes for columns used for search

--- src/Entity/NotFoundLog.php
+++ src/Entity/NotFoundLog.php
@@ -4,30 +4,34 @@ declare(strict_types=1);
 
 namespace ThreeBRS\Sylius404LogPlugin\Entity;
 
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'three_brs_404_not_found_log')]
+#[ORM\Index(columns: ['url_domain'])]
+#[ORM\Index(columns: ['url_slug'])]
+#[ORM\Index(columns: ['created_at'])]
 class NotFoundLog implements NotFoundLogInterface
 {
     #[ORM\Id]
-    #[ORM\Column(type: 'integer')]
+    #[ORM\Column(type: Types::INTEGER)]
     #[ORM\GeneratedValue(strategy: 'AUTO')]
     protected ?int $id = null;
 
-    #[ORM\Column(type: 'string', name: 'url_domain')]
+    #[ORM\Column(name: 'url_domain', type: Types::STRING)]
     protected string $urlDomain;
 
-    #[ORM\Column(type: 'text', name: 'url_slug')]
+    #[ORM\Column(name: 'url_slug', type: Types::STRING, length: 765)]
     protected string $urlSlug;
 
-    #[ORM\Column(type: 'text', name: 'query_string', nullable: true)]
+    #[ORM\Column(name: 'query_string', type: Types::TEXT, nullable: true)]
     protected ?string $queryString = null;
 
-    #[ORM\Column(type: 'text', name: 'user_agent', nullable: true)]
+    #[ORM\Column(name: 'user_agent', type: Types::TEXT, nullable: true)]
     protected ?string $userAgent = null;
 
-    #[ORM\Column(type: 'datetime_immutable', name: 'created_at')]
+    #[ORM\Column(name: 'created_at', type: Types::DATETIME_IMMUTABLE)]
     protected \DateTimeImmutable $createdAt;
 
     public function __construct()
